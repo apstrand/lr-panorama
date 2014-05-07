cce8d853a11171fdcae64ad11b807be061310c57
Peter Strand <peter@nena.se>
2014-05-07 09:12:57 +0200

    Based on hello world plugin from the sdk

---
+++ Info.lua
@@ -0,0 +1,14 @@
+
+return {
+	
+	LrSdkVersion = 5.0,
+	LrSdkMinimumVersion = 5.0,
+	LrToolkitIdentifier = 'se.zarquon.panoexport',
+	LrPluginName = LOC "$$$/PanoramaExport/PluginName=Panorama Export",
+	LrExportMenuItems = {
+		title = "Panorama Export",
+		file = "ExportMenuItem.lua",
+	},
+	VERSION = { major=0, minor=0, revision=1, build=1, },
+}
+
