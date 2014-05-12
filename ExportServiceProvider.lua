79b53efcc7b5aba3dbdec3d5a86fac2b87fa7599
Peter Strand <peter@nena.se>
2014-05-12 10:24:37 +0200

    add dummy ExportServiceProvider

---
+++ ExportServiceProvider.lua
@@ -0,0 +1,46 @@
+
+dump = require 'ser'
+
+local LrLogger = import 'LrLogger'
+
+local logger = LrLogger( 'exportLogger' )
+logger:enable( "print" ) -- Pass either a string or a table of actions.
+
+
+local function log( message )
+  logger:trace( " PanoExport: " .. message )	
+end
+
+
+
+return {
+
+  startDialog = function(propertyTable)
+    log("startDialog: ")
+  end,
+
+  endDialog = function(propertyTable, why)
+    log("endDialog: " .. why)
+  end,
+
+  exportPresetFields = {
+    { key = 'myPluginSetting', default = 'Initial value' }
+  },
+
+  showSections = { 'exportLocation', 'fileNaming', 'imageSettings' },
+
+  updateExportSettings = function (exportSettings)
+    log("updateExportSettings: " .. exportSettings)
+    exportSettions['LR_collisionHandling'] = 'overwrite'
+  end,
+
+  processRenderedPhotos = function(functionContext, exportContext)
+    local exports = { }
+    for i,rendition in exportContext:renditions() do
+      local success, pathOrMessage = rendition:waitForRender()
+      log("rendition " .. i .. " " .. tostring(success) .. " " .. pathOrMessage)
+      table.insert(exports, pathOrMessage)
+    end
+    log("photos: " .. tostring(exports))
+  end
+}
